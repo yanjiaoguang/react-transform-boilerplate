98e12eb7bf39f810807adb4e121eef3a4e590086
yanjg <1336749157@qq.com>
2017-01-16 12:24:50 +0800

    i

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -6,12 +6,30 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 
 class NavBar extends Component {
   render() {
+    let styles = {
+      tabs: {
+        width: '390px',
+        position: 'absolute',
+        right: '60px',
+        textTransform: 'uppercase',
+      },
+      tab: {
+        height: '64px',
+        color: '#fff',
+      },
+      inkBar: {
+        height: '4px',
+        marginTop: '-4px',
+      },
+    };
     return (
-          <Tabs>
-            <Tab label="Home" />
-            <Tab label="Home" />
-            <Tab label="About" />
+      <div className="app-header">
+          <Tabs tabItemContainerStyle={{ backgroundColor: 'transparent' }} style={styles.tabs} inkBarStyle={styles.inkBar}>
+            <Tab style={styles.tab} label="Home" />
+            <Tab style={styles.tab} label="Account" />
+            <Tab style={styles.tab} label="About" />
           </Tabs>
+      </div>
     );
   }
 }
